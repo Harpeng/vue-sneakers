e94d626962fe8cfac38f9ee06f5c8ba5c2a2fe25
Harpeng <gondarev75@gmail.com>
2024-06-11 23:53:11 +0300

    layout header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue'
+</script>
 
 <template>
-  <div></div>
+  <div class="h-screen w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14">
+    <Header />
+    <div>
+      <div>
+        <img src="/like-2.svg" alt="like">
+        <img src="/sneakers/sneakers-1.jpg" alt="sneaker">
+        <p>Мужские кроссовки Nike blazer Mid suede</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="logo" class="w-10" />
+      <div>
+        <h1 class="text-xl font-bold uppercase">Vue Sneakers</h1>
+        <p class="text-slate-500">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 text-slate-500 hover:text-black cursor-pointer">
+        <img src="/cart.svg" alt="cart" />
+        <span>1205 руб</span>
+      </li>
+      <li class="flex items-center gap-3 text-slate-500 hover:text-black cursor-pointer">
+        <img src="/heart.svg" alt="bookmark" />
+        <span>Закладки</span>
+      </li>
+      <li class="flex items-center gap-3 text-slate-500 hover:text-black cursor-pointer">
+        <img src="/profile.svg" alt="profile" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
