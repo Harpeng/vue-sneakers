ba1bb3c716a210ee8060d18b9ac90aae02a24dad
Harpeng <gondarev75@gmail.com>
2024-06-16 22:34:16 +0300

    feature/animate

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -11,7 +11,7 @@ const emit = defineEmits(['addToFavorite', 'addToCart'])
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-5 ">
+  <div v-auto-animate class="grid grid-cols-4 gap-5 ">
     <Card
       v-for="item in items"
       :id="item.id "
